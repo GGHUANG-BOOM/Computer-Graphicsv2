4bcb0fd59cc7886261bd6300e1ed079867f2db87
IT <142363330+RinreiRay@users.noreply.github.com>
2025-05-31 14:17:57 +1000

    view

--- test.js
+++ test.js
@@ -63,7 +63,7 @@ function init() {
   scene.background = new THREE.Color(0x333333);
 
   const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
-  camera.position.set(0, 2, 5);
+  camera.position.set(-5, 2, 0);
 
   const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
   renderer.setSize(window.innerWidth, window.innerHeight);
