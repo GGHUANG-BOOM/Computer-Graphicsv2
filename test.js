3337c4342401845e05edd91937ac0c091c204e3d
GGHUANG-BOOM <gordon.g.huang@student.uts.edu.au>
2025-05-31 14:20:44 +1000

    update

--- test.js
+++ test.js
@@ -4,6 +4,7 @@ import { MTLLoader } from './MTLLoader.js';
 import { OBJLoader } from './OBJLoader.js';
 import { GLTFLoader } from './GLTFLoader.js';
 //import { mannequincontrols } from './mannequincontrols.js';
+import { Water } from '/objects/Water2.js';
 
 // Page Navigation
 const startButton = document.getElementById('start-button');
@@ -255,6 +256,41 @@ loader.load('beach Scene/uploads_files_5954063_beach+scene.glb', function (gltf)
     });
   });
   
+
+  const waterGeometry = new THREE.PlaneGeometry(50, 50);
+  const waterNormals = new THREE.TextureLoader().load('Textures/Water_2_M_Normal.jpg', function (texture) {
+    texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+  });
+  
+  const water = new Water(waterGeometry, {
+    textureWidth: 512,
+    textureHeight: 512,
+    waterNormals: waterNormals,
+    sunDirection: new THREE.Vector3(),
+    sunColor: 0xffffff,
+    waterColor: 0x001e0f,
+    distortionScale: 3.7,
+    fog: scene.fog !== undefined
+  });
+  
+  water.rotation.x = -Math.PI / 2;
+  water.position.set(5, 0.01, 8); // Adjust to align with beach
+  scene.add(water);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   window.wearShirt = function () {
     if (!mannequin) return;
